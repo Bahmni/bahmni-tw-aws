fb94463d890a5457e56814f372d4d8ca3d30fddd
Vinay Venu <vinayv@thoughtworks.com>
2016-10-27 14:43:38 +0530

    Fix vpc creation. Read on. 1. Create a script to create vpc. 2. Change circular dependency between security_group role and cidr_blocks. 3. Add todo items on vpc readme.md.

---
+++ roles/vpc/Readme.md
@@ -0,0 +1,8 @@
+TODO:
+---------------
+Wait for ansible 2.2 and
+ -  Get rid of all aws cli calls
+ -  Move everything to just one play
+ -  security_group.yml - try running in a loop
+
+
---
+++ scripts/create_vpc.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#This tells you stuff that you need on your machine in order to run the plays mentioned.
+
+TEMP_SCRIPT_DIR=`dirname -- "$0"`
+SCRIPT_DIR=`cd $TEMP_SCRIPT_DIR; pwd`
+cd $SCRIPT_DIR/..
+
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+printf "${RED}Note: If this script fails, please take a look at the contents and uncomment lines that you think
+may be required${NC}\n"
+
+
+# Ansible - Ensure you have something >2.1
+#brew install ansible
+
+# Python modules that we use
+# pip install boto
+# pip install awscli
+
+# echo "Configuring aws-cli. Remember to remove this step once we move to ansible 2.2. "
+# echo "Please decrypt secure_vars.yml to find the right values"
+# aws-configure
+
+ansible-playbook -i vpc_inventory vpc.yml
+
+
+
+
