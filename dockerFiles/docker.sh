025e0b57a66322e3fd63c7493420bdda02594fe9
senthilr <senthilr@thoughtworks.com>
2017-05-18 12:41:54 +0530

    fixed build-arg issue in docker.sh

--- dockerFiles/docker.sh
+++ dockerFiles/docker.sh
@@ -4,5 +4,5 @@ sudo chkconfig docker on
 sudo service docker start
 sudo docker login -u $hub_username -p $hub_password
 for container_id in $(docker ps  --filter="name=$container_name" -q);do docker stop $container_id && docker rm $container_id;done
-sudo docker build -t senthilrajar/bahmni_centos:${rpm_version} --build-arg rpm_version=${rpm_version} aws_secret_key=${aws_secret_key} container_name=${container_name} aws_access_key=${aws_access_key} .
+sudo docker build -t senthilrajar/bahmni_centos:${rpm_version} --build-arg rpm_version=${rpm_version} --build-arg aws_secret_key=${aws_secret_key} --build-arg container_name=${container_name} --build-arg aws_access_key=${aws_access_key} .
 sudo docker run -it -d -p 8080:80 -p 9090:443 -p 3306:3306 -p 5432:5432 -p 8050:8050 -p 8069:8069 -p 8052:8052 --privileged --name $container_name senthilrajar/bahmni_centos:${rpm_version} /bin/bash
