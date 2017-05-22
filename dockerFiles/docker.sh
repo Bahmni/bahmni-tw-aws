c8c24078c8a4e11501673c641a8c5923851c9609
hanishar <hanisha946@gmail.com>
2017-05-22 17:02:32 +0530

    Hanisha | Docker.sh

--- dockerFiles/docker.sh
+++ dockerFiles/docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 container_name=$container_name
-sed -i '1i '$ansible_vault_key'' /var/go/.keys/bahmni-tw-aws.pem
+sed -i '1i \'$ansible_vault_key'\' /var/go/.keys/bahmni-tw-aws.pem
 sed -i '2d' /var/go/.keys/bahmni-tw-aws.pem
 sudo yum install -y docker
 sudo chkconfig docker on
