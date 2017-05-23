25d11362b124c492f120c380e5416761fb3680ef
hanishar <hanisha946@gmail.com>
2017-05-23 12:11:26 +0530

    Hanisha | Docker.sh

--- dockerFiles/docker.sh
+++ dockerFiles/docker.sh
@@ -16,7 +16,7 @@ if sudo docker ps | awk -v container_name="${container_name}" 'NR>1{($(NF) == co
    sudo docker stop "${container_name}" && sudo docker rm -f "${container_name}"
 fi
 cd ${base_path}/${GO_PIPELINE_NAME}/bahmni-tw-aws
-ansible-playbook -i inventory/ docker_instance.yml --extra-vars="container_name=$container_name" --extra-vars="https_port=$https_port"  --extra-vars="erp_port=$erp_port" --extra-vars="aws_access_key=$aws_access_key" --extra-vars="aws_secret_key=$aws_secret_key" -vvv
+ansible-playbook -i inventory/ docker_instance.yml --vault-password-file ~/.keys/bahmni-tw-aws.pem --extra-vars="container_name=$container_name" --extra-vars="https_port=$https_port"  --extra-vars="erp_port=$erp_port" --extra-vars="aws_access_key=$aws_access_key" --extra-vars="aws_secret_key=$aws_secret_key" -vvv
 cd ..
 sudo docker build -t senthilrajar/bahmni_centos:${rpm_version} --build-arg rpm_version=${rpm_version} --build-arg aws_secret_key=${aws_secret_key} --build-arg container_name=${container_name} --build-arg aws_access_key=${aws_access_key} .
 sudo docker run -it -d -p ${https_port}:443 -p ${erp_port}:8069 --privileged --name $container_name senthilrajar/bahmni_centos:${rpm_version} /bin/bash
