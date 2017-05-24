cf0b1e8a05f4106106ac724cc71c080d7ad0aa8d
hanishar <hanisha946@gmail.com>
2017-05-24 12:43:13 +0530

    Hanisha | Reverting changes for creating new docker user

--- dockerFiles/docker.sh
+++ dockerFiles/docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 container_name=$container_name
-sed -i '1i \'$aws_vault_key'\' /var/go/.keys/bahmni-tw-aws.pem
-sed -i '2d' /var/go/.keys/bahmni-tw-aws.pem
 sudo yum install -y docker
 sudo chkconfig docker on
 sudo service docker start
@@ -15,8 +13,6 @@ fi
 if sudo docker ps | awk -v container_name="${container_name}" 'NR>1{($(NF) == container_name)}'; then
    sudo docker stop "${container_name}" && sudo docker rm -f "${container_name}"
 fi
-cd ${base_path}/${GO_PIPELINE_NAME}/bahmni-tw-aws
-ansible-playbook -i inventory/ docker_instance.yml --vault-password-file ~/.keys/bahmni-tw-aws.pem --extra-vars="container_name=$container_name" --extra-vars="https_port=$https_port"  --extra-vars="erp_port=$erp_port" --extra-vars="aws_access_key=$aws_access_key" --extra-vars="aws_secret_key=$aws_secret_key" -vvv
-cd ..
+
 sudo docker build -t senthilrajar/bahmni_centos:${rpm_version} --build-arg rpm_version=${rpm_version} --build-arg aws_secret_key=${aws_secret_key} --build-arg container_name=${container_name} --build-arg aws_access_key=${aws_access_key} .
 sudo docker run -it -d -p ${https_port}:443 -p ${erp_port}:8069 --privileged --name $container_name senthilrajar/bahmni_centos:${rpm_version} /bin/bash
