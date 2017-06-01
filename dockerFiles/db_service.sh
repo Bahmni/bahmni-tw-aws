2fc153ed3590d26a6c8b208043460d873b0ea241
senthilr <senthilr@thoughtworks.com>
2017-06-01 13:15:22 +0530

    Modify mysql and pgsql datadirectory

--- dockerFiles/db_service.sh
+++ dockerFiles/db_service.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+container_name=$container_name
 
 if (( $(ps -ef | grep -v grep | grep mysqld | wc -l) > 0 ))
 then
@@ -13,4 +14,23 @@ if [ "$?" -gt "0" ]; then
   echo "Not installed".
 else
   sudo service postgresql-9.2 start
+fi
+
+if [ -f /etc/my.cnf ]; then
+    sudo mkdir -p /${container_name}/mysql && sudo chown -R mysql:mysql /${container_name}/mysql
+    sudo sed -i "s|datadir=/var/lib/mysql|datadir=/${container_name}/mysql|g" /etc/my.cnf
+    sudo rsync -avr -o -g /var/lib/mysql /${container_name}
+    sudo service mysqld restart
+else
+   echo "File /etc/my.cnf does not exist."
+fi
+
+if [ -f /var/lib/pgsql/.bash_profile ]; then
+    sudo mkdir -p /${container_name}/pgsql/9.2/data && sudo chown -R postgres:postgres /${container_name}/pgsql
+    sudo sed -i "s|PGDATA=/var/lib/pgsql/9.4/data|PGDATA=/${container_name}/pgsql/9.2/data|g" /var/lib/pgsql/.bash_profile
+    export PGDATA
+    sudo rsync -avr -o -g /var/lib/pgsql /${container_name}
+    sudo service postgresql-9.2 restart
+else
+   echo "File /var/lib/pgsql/.bash_profile does not exist."
 fi
