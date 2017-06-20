a2281cdfc301c1fab87f72a1a94717e14e6e6280
senthilr <r.senthilreddy@gmail.com>
2017-06-20 09:49:55 +0530

    Update docker.sh

--- dockerFiles/docker.sh
+++ dockerFiles/docker.sh
@@ -19,7 +19,8 @@ else
 fi
 
 if sudo docker images | grep ${container_name}; then
-   sudo docker rmi (docker images | grep ${container_name} | awk '{print $3}')
+   sudo docker images | grep ${container_name} | awk '{print $1}' | xargs docker rm
+#   sudo docker rmi (docker images | grep ${container_name} | awk '{print $3}')
 else
   echo "Image doesn't exists"
 fi
