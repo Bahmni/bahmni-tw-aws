3cee8fc6a2f84760b06e28551b6f508baa26a9a7
senthilr <r.senthilreddy@gmail.com>
2017-06-20 18:39:37 +0530

    changed docker images path

--- dockerFiles/docker.sh
+++ dockerFiles/docker.sh
@@ -29,5 +29,5 @@ if ! sudo docker volume ls -q --filter name="${container_name}"| grep -q "${cont
 else
         echo "Volume ${container_name} exists"
 fi
-sudo docker build --rm -t senthilrajar/bahmni_centos:${container_name} --build-arg rpm_version=${rpm_version} --build-arg inventory_name=${inventory_name} --build-arg aws_secret_key=${aws_secret_key} --build-arg container_name=${container_name} --build-arg aws_access_key=${aws_access_key} .
-sudo docker run -e container_name=${container_name} -it -d -p ${https_port}:443 --privileged --name $container_name -v $container_name:/$container_name senthilrajar/bahmni_centos:${container_name} /bin/bash
+sudo docker build --rm -t bahmni/bahmni_centos:${container_name} --build-arg rpm_version=${rpm_version} --build-arg inventory_name=${inventory_name} --build-arg aws_secret_key=${aws_secret_key} --build-arg container_name=${container_name} --build-arg aws_access_key=${aws_access_key} .
+sudo docker run -e container_name=${container_name} -it -d -p ${https_port}:443 --privileged --name $container_name -v $container_name:/$container_name bahmni/bahmni_centos:${container_name} /bin/bash
